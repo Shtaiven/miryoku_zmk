2cf04d6b282958fb099e90236f10620dc7d21978
Steven Eisinger <steven.a.eisinger@gmail.com>
2024-04-29 22:54:48 +0200

    feat: mouse keys and custom_config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,8 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
+//
+// Modified 2024 by Steven Eisinger
 
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_TAP_COLEMAKDHK
+#define MIRYOKU_NAV_INVERTEDT
