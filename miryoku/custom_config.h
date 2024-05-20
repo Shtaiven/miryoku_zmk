4f0a6c404ff00b59419b2bd8be78248babdb451f
Steven Eisinger <steven.a.eisinger@gmail.com>
2024-05-20 15:20:34 +0200

    feat: added nice_view build

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,3 +6,15 @@
 #define MIRYOKU_ALPHAS_QWERTY
 #define MIRYOKU_TAP_COLEMAKDHK
 #define MIRYOKU_NAV_INVERTEDT
+
+// TODO: Shift on thumb key ??
+// TODO: ESC on left pinkie ??
+// TODO: TAB on left pinkie ??
+// TODO: personalized nav layer ??
+// TODO: colon on right pinkie ??
+// TODO: backslash on right pinkie ??
+// TODO: replace mouse layer with something else or get it to work
+// TODO: personalized sym layer
+// TODO: gaming mode with left hand shifted by 1 and tap only with traditional layout
+// TODO: holdable backspace
+// TODO: timerless homerow mods (might require thumb shift)
